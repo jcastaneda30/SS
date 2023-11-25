29b5d91940ce8aadd739aa481cbf574ccaa10aaa
jcastaneda30 <jaidercasta30@gmail.com>
2023-11-24 21:19:40 -0500

    Separacion por estante y restaurante

--- R/Distribuciones/Pruebas.R
+++ R/Distribuciones/Pruebas.R
@@ -74,34 +74,63 @@ ppcomp (list(fit_normal,fit_unif, fit_gamma), legendtext = plot.legend)
 
 
 
-datosTiempoServicio <- read.table("TiemposDeServicioSec.txt")
+datosTiempoServicio <- read.table("TiemposDeServicioSec.txt",sep = ",")
 datosTiempoServicio$V1 <- as.numeric(datosTiempoServicio$V1)
-mean(datosTiempoServicio$V1)
-plotdist(datosTiempoServicio$V1,histo=TRUE,demp=TRUE)
-descdist(datosTiempoServicio$V1, boot = 1000 )
 
-fit_normal <- fitdist(datosTiempoServicio$V1,"norm")
-fit_unif <- fitdist(datosTiempoServicio$V1,"unif")
-fit_gamma <- fitdist(datosTiempoServicio$V1,"gamma")
-fit_beta <- fitdist(datosTiempoServicio$V1,"beta")
+datosEstante <- datosTiempoServicio[datosTiempoServicio$V2=="estante",]
 
-plot(fit_normal)
-plot(fit_unif)
+
+mean(datosEstante$V1)
+plotdist(datosEstante$V1,histo=TRUE,demp=TRUE)
+descdist(datosEstante$V1, boot = 1000 )
+
+fit_gamma <- fitdist(datosEstante$V1,"gamma")
+fit_exp <- fitdist(datosEstante$V1,"exp")
+
 plot(fit_gamma)
+plot(fit_exp)
 
-summary(fit_normal)
-summary(fit_unif)
 summary(fit_gamma)
+summary(fit_exp)
 
-gofstat(list(fit_normal,fit_unif))
+gofstat(list(fit_gamma,fit_exp))
 
-#La distribucion uniforme es descartada por kolmogorov smirnov, y
+#La distribucion exp es descartada por kolmogorov smirnov, y
 #por criterio de informacion se selecciona la distribucion gamma
 par(mfrow=c(2,2))
-plot.legend <- c("normal","uniforme","gamma")
-denscomp(list(fit_normal,fit_unif, fit_gamma), legendtext = plot.legend)
-cdfcomp (list(fit_normal,fit_unif, fit_gamma), legendtext = plot.legend)
-qqcomp (list(fit_normal,fit_unif, fit_gamma), legendtext = plot.legend)
-ppcomp (list(fit_normal,fit_unif, fit_gamma), legendtext = plot.legend)
+plot.legend <- c("gamma","exp")
+denscomp(list(fit_gamma,fit_exp), legendtext = plot.legend)
+cdfcomp (list(fit_gamma,fit_exp), legendtext = plot.legend)
+qqcomp (list(fit_gamma,fit_exp), legendtext = plot.legend)
+ppcomp (list(fit_gamma,fit_exp), legendtext = plot.legend)
+
+
+par(mfrow=c(1,1))
+
+datosRestaurante <- datosTiempoServicio[datosTiempoServicio$V2=="restaurante",]
+
+mean(datosRestaurante$V1)
+plotdist(datosRestaurante$V1,histo=TRUE,demp=TRUE)
+descdist(datosRestaurante$V1, boot = 1000 )
+
+fit_gamma <- fitdist(datosRestaurante$V1,"gamma")
+fit_ln <- fitdist(datosRestaurante$V1,"lnorm")
+
+plot(fit_gamma)
+plot(fit_ln)
+
+summary(fit_gamma)
+summary(fit_ln)
+
+gofstat(list(fit_gamma,fit_ln))
+
+#La distribucion lognormal es descartada por kolmogorov smirnov, y
+#por criterio de informacion se selecciona la distribucion gamma
+par(mfrow=c(2,2))
+plot.legend <- c("gamma","log normal")
+denscomp(list(fit_gamma,fit_ln), legendtext = plot.legend)
+cdfcomp (list(fit_gamma,fit_ln), legendtext = plot.legend)
+qqcomp (list(fit_gamma,fit_ln), legendtext = plot.legend)
+ppcomp (list(fit_gamma,fit_ln), legendtext = plot.legend)
 
 
